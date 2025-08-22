46caf25a536f98f068cc64d2739a53994caf0be1
Tommaso Iovane <tommaso.iovane@sinapto.com>
2025-08-22 10:14:19 +0000

    pwa config

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import { registerSW } from 'virtual:pwa-register'
+
 
 const app = mount(App, {
     target: document.getElementById('app'),
 })
 
-
+const updateSW = registerSW({
+    immediate: true
+})
 
 export default app
